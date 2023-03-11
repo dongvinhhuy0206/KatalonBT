11406669a453b3daad010a8934196bb26e0397b8
dongvinhhuy0206 <dongvinhhuy0206@gmail.com>
2023-03-11 12:05:40 +0700

    Add BT Fahasa

---
+++ Object Repository/Fahasa/btn_DangNhap.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_DangNhap</name>
+   <tag></tag>
+   <elementGuidId>ed742b3f-cd84-45f8-bf18-444dd497692e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@class=&quot;fhs-btn-login&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Fahasa/txt_MatKhau.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>txt_MatKhau</name>
+   <tag></tag>
+   <elementGuidId>fc20f639-5d41-4037-a5db-15fda9e8dc93</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@id=&quot;login_password&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
